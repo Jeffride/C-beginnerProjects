2cd8eacdab9b951eb8cc695b9d22de7f1548d3e1
Jeffride <attridej@gmail.com>
2019-05-22 01:09:55 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace myFirstApp
+{
+    class main{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+           
+
+        }
+
+    }
+}
